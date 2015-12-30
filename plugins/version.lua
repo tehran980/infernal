eaff5480d8c24e3615122893e44c285b771888fe
FLUXTERTEAM <arashmardan.am@gmail.com>
2015-12-30 08:58:44 -0800

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,9 +1,22 @@
 do
 
 function run(msg, matches)
-  return 'TeleSwwit '.. VERSION .. [[ 
-  Checkout http://git.io/v4Oi0
-  GNU GPL v2 license.]]
+  return 'Infernal'.. VERSION ..[[
+  'FLUXTER Team Robot v1.3'.. [[ 
+  Website:
+  http://FLUXTERTEAM.epage.IR
+  Channel: @FLUXTERteam
+  Creator: @ArashFluxter [sudo1]
+  Developer: @ArashFluxter
+  Founder:@shahabsaf [Sudo2]
+  Manager : @Parham_Fluxter [Sudo3]
+  Admins
+  @mustafaFLux
+  @Creed_is_Dead
+  Powered by:
+  ArashFluxter security. Group
+  http:// soon :D
+  Engineer Arash.m]]
 end
 
 return {
