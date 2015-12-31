ea1fe7e501049bf0d01ad20f0331bf9f0632ac19
FLUXTERTEAM <arashmardan.am@gmail.com>
2015-12-31 06:24:16 -0800

    Update and rename arashfuxter.lua to arashinfernal.lua

---
+++ plugins/arashinfernal.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return "ArashINFERNAl Is King OF bots and Developers😍 .. matches[1]
+end
+
+return {
+  description = "Says About arashinfernal", 
+  usage = "say about ArashINFERNAl",
+  patterns = {
+    "^arashINFERNAL$",
+    "^Arashinfernal$"
+  }, 
+  run = run 
+}
+
+end
