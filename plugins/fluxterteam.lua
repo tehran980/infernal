4d399035abcfb0492cbda77b6b6f0dbc900db27e
FLUXTERTEAM <arashmardan.am@gmail.com>
2015-12-31 06:19:44 -0800

    Create fluxterteam.lua

---
+++ plugins/fluxterteam.lua
@@ -0,0 +1,30 @@
+do
+
+function run(msg, matches)
+  return ''.. [[ 
+  The internasional Security TEAM😱
+INFERNAL™
+〰〰〰〰〰〰〰〰〰〰〰〰
+Leader : @ArashInfernal
+moderator : @Shahabsaf 
+moderator : @creed_is_dead 
+moderator : @Mustafaflux 
+〰〰〰〰〰〰〰〰〰〰〰〰
+Member : @NXT_Sudo 
+Member : @Arash_admin_dragon 
+Member : @lord_hpr 
+Member : @ali_ghoghnoos
+Member : @Minaco
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "version: Shows bot version",
+  patterns = {
+    "^fluxterteam$"
+    "
+  }, 
+  run = run 
+}
+
+end
