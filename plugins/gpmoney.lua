571e4fcfa3731f91be3275f038e0c8c9186c1a79
FLUXTERTEAM <arashmardan.am@gmail.com>
2015-12-31 05:30:20 -0800

    Create gpmoney.lua

---
+++ plugins/gpmoney.lua
@@ -0,0 +1,23 @@
+do
+
+function run(msg, matches)
+  return best anti Spam bot😳
+best manager of your group😱😱😱😱
+for a group You can give us something
+like adding members to  our support
+or you can pay 1 tomans each month
+our you can join or channel for a group 
+@FLUXTERchannel .. matches[1]
+end
+
+return {
+  description = "Says hello to someone", 
+  usage = "say fuck to [name]",
+  patterns = {
+    "^gpmoney$",
+    "^Say fuck to (.*)$"
+  }, 
+  run = run 
+}
+
+end
