6bf23bf1459846c1001bdf5ee6fe32d79c9aebbe
FLUXTERTEAM <arashmardan.am@gmail.com>
2015-12-31 05:11:02 -0800

    Create chat1.lua

---
+++ plugins/chat1.lua
@@ -0,0 +1,16 @@
+do
+
+function run(msg, matches)
+  return "زياد سوال ميکنيــــــــــــــــــــا ..matches [1]       
+
+return {
+  description = "ziyad soal mikoni a", 
+  usage = "no help",
+  patterns = {
+    "^?$",
+    "^?$",
+  }, 
+  run = run 
+}
+
+end
