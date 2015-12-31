ecb438b9e4cf47eda9406d23a4ca605e4add97e8
FLUXTERTEAM <arashmardan.am@gmail.com>
2015-12-31 05:34:46 -0800

    Create arashfuxter.lua

---
+++ plugins/arashfuxter.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return "ArashFluxter Is King OF bots and Developers😍 .. matches[1]
+end
+
+return {
+  description = "Says About ArashFluxter", 
+  usage = "say about ArashFLuxter  ",
+  patterns = {
+    "^arashFluxter$",
+    "^ArashFluxter$"
+  }, 
+  run = run 
+}
+
+end
